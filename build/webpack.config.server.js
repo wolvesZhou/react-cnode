db21bb15294f66a088a811edb224319aba723ea7
周炜烽 <zhouwf@dxy.cn>
2019-10-28 12:11:49 +0800

    chore： 添加eslint校验

--- build/webpack.config.server.js
+++ build/webpack.config.server.js
@@ -15,6 +15,14 @@ module.exports = {
   module: {
     rules: [
       {
+        enforce: 'pre',
+        test: /.(js|jsx$/,
+        loader: 'eslint-loader',
+        exclude: [
+          path.resolve(__dirname, '../node_modules')
+        ]
+      },
+      {
         test: /.jsx$/,
         loader: 'babel-loader'
       },
--- client/app.js
+++ client/app.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.jsx';
 import { AppContainer } from 'react-hot-loader';
+import App from './App.jsx';
 
 // 要用AppContainer去包裹我们根节点去渲染的内容
 const root = document.getElementById('root');
-const render = Component => {
+const render = (Component) => {
   ReactDOM.hydrate(
     <AppContainer>
       <Component />
     </AppContainer>,
-    root
-  )
-}
+    root,
+  );
+};
 
 render(App);
 
 if (module.hot) {
   module.hot.accept('./App.jsx', () => {
+    // eslint-disable-next-line global-require
     const NextApp = require('./App.jsx').default;
     render(NextApp);
-  })
+  });
 }
